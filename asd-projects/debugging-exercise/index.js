d4e0f9f8b8bca8eaabd579bef6b63b03b8e92ae8
remyreeves <reeves165@wearescpps.org>
2025-01-30 17:41:48 +0000

    progress

--- asd-projects/debugging-exercise/index.js
+++ asd-projects/debugging-exercise/index.js
@@ -63,11 +63,11 @@ function decideSpeed(){
 
 // this generates an id for a circle given the circle's number
 function getId(number){
-    return "circle" + number; ()
+    return ("circle" + number);
 }
 
 // this adds a circle into the HTML
-funtion addNewCircleElement(circle, id){
+function addNewCircleElement(circle, id){
 
     // this creates the HTML for a new circle element 
     var $circle = $('<div>').attr('id', id)
@@ -84,7 +84,7 @@ funtion addNewCircleElement(circle, id){
 //////////////////
 
 // this should move all of the circles
-function update){
+function update(){
 
     // loop over the circles array. We use the maxCircles variable instead of circles.length
     // to make seeing issues in the debugger slightly easier (in practice, you should use
@@ -117,7 +117,7 @@ function moveCircle(circle){
 function bounceCircle(circle){
 
     // this bounces off the left wall
-    if (circle.x < 0{
+    if (circle.x < 0){
         circle.x -= circle.speedX;
         circle.speedX *= -1;
     }
